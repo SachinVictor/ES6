6aa1b75a76d35f276e24f8d043131e1323dfbb2a
SachinVictor <168399831+SachinVictor@users.noreply.github.com>
2024-04-29 18:55:42 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,10 @@
+import square,{add,multiply} from "./mathOperations.js"; //import functions from mathoperations.js
+
+var addResult = add(10,5); //invoke function add and log the result
+console.log(addResult);
+
+var multiplyResult = multiply(4,3); //invoke function multiply  and log the result
+console.log(multiplyResult);
+
+var squreResult = square(9); //invoke function square and log the result
+console.log(squreResult);
